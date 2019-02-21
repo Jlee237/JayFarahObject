ff2a68a67ec46ef36958454b9063b0ece804e322
Farah Student <farah.gus@hotmail.com>
2019-02-21 11:57:02 -0500

    keyDeleter function

--- object.js
+++ object.js
@@ -32,3 +32,17 @@ function personMaker() {
 }
 
 personMaker();
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+
+  return obj;
+}
