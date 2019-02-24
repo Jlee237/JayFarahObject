ed2d3ca7cbf5cfb7d914e4e3f7916e094ae40491
Farah Student <farah.gus@hotmail.com>
2019-02-24 15:22:09 -0500

    finished LOTR function

--- objectThree.js
+++ objectThree.js
@@ -33,6 +33,9 @@ const food = {
 console.log(food.meals[3]);
 
 
+//==================================================
+// ARRAY OF OBJECTS AND PROPERTIES THAT AREN'T THERE
+//==================================================
 const people = [{
   name: 'Jay',
   jobTitle: 'Owner',
@@ -55,82 +58,77 @@ for (let i=0; i< people.length; i++) {
   }
 }
 
-
-function createCharacter(){
-  const Gandalf = {
-    name: 'Gandalf the White',
-    nickname: 'gandalf',
-    race: 'wizard',
-    origin: 'Middle Earth',
-    attack: 10,
-    defense: 6,
-    describe: function() {
-      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
-    },
-    evaluateFight: function(character){
-      return `Your opponent takes ${x} and you recieve ${y} damage.`;
-    } 
+//===================================
+// CRACKING THE CODE
+//===================================
+function decode(words){
+  const cipher = {
+    a: 2, 
+    b: 3, 
+    c: 4,
+    d: 5
   };
 
-  const Bilbo = {
-    name: 'Bilbo Baggins',
-    nickname: 'bilbo',
-    race: 'Hobbit',
-    origin: 'The Shire',
-    attack: 2,
-    defense: 1,
-    describe: function() {
-      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
-    },
-    evaluateFight: function(character){
-      return `Your opponent takes ${x} and you recieve ${y} damage.`;
-    } 
-  };
+  let wordArr = words.split(' ');
+  let decodedWords = wordArr.map(word => {
+    let newLetter;
+    if (cipher[word[0]]){
+      newLetter = cipher[word[0]] - 1; 
+    }
+    
+    return newLetter ? word[newLetter] : ' '; 
+  });
 
-  const Frodo = {
-    name: 'Frodo Baggins',
-    nickname: 'frodo',
-    race: 'Hobbit',
-    origin: 'The Shire',
-    attack: 3,
-    defense: 2,
-    describe: function() {
-      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
-    },
-    evaluateFight: function(character){
-      return `Your opponent takes ${x} and you recieve ${y} damage.`;
-    } 
-  };
+  return decodedWords.join('');
+}
 
-  const Aragorn = {
-    name: 'Aragorn son of Arathorn',
-    nickname: 'aragorn',
-    race: 'man',
-    origin: 'Dunnedain',
-    attack: 6,
-    defense: 8,
-    describe: function() {
-      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
-    },
-    evaluateFight: function(character){
-      return `Your opponent takes ${x} and you recieve ${y} damage.`;
-    } 
-  };
+function decodeWOrds(){
+}
 
-  const Legolas = {
-    name: 'Legolas',
-    nickname: 'legolas',
-    race: 'elf',
-    origin: 'Woodland Realm',
-    attack: 8,
-    defense: 5,
-    describe: function() {
-      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+
+//===================================
+// LOTR
+//===================================
+function createCharacter(name, nickname, race, origin, attack, defense){
+  return {
+    name, nickname, race, origin, attack, defense,
+    desrcibe: function(){
+      console.log(`${name} is a ${race} from ${origin}.`);
     },
-    evaluateFight: function(character){
-      return `Your opponent takes ${x} and you recieve ${y} damage.`;
-    } 
+
+    evaluateFight: function(character){ 
+      let damageGiven = 0;
+      let damageReceived = 0; 
+      if (this.attack > character.defense) {
+        damageGiven = this.attack - character.def;
+      }
+      if (this.def < character.attack){
+        damageReceived = character.attack - this.defense;
+      }
+
+      console.log(`Your opponent takes ${damageGiven} damage and you receive ${damageReceived} damage.`); 
+    },
   };
 }
 
-const characters = [createCharacter()];
+// array of characters
+const characters = [
+  createCharacter('Gandalf the White', 'Gandalf', 'Wizard', 'Middle Earth', 10, 6),
+  createCharacter('Bilbo Baggins', 'biblo', 'hobbit', 'The Shire', 2, 1),
+  createCharacter('Frodo Baggins', 'Frodo', 'hobbit', 'The Shire', 3, 2),
+  createCharacter('Aragorn son of Arathorn', 'man', 'Wizard', 'Dunnedain', 6, 8),
+  createCharacter('Legolas', 'Legolas', 'elf', 'Woodland Realm', 8, 5)
+];
+
+// add character to array
+characters.push(createCharacter('Arwen Undomiel', 'arwen', 'half-elf', 'Rivendell', 4, 5));
+
+// find character 
+characters.find(character => character.nickname === 'aragorn').desrcibe();
+
+// filter character for hobbits
+let hobbitArray = characters.filter(character => character.race === 'hobbit');
+
+// filter characters with attack > 5
+let strongCharacters = characters.filter(character => character.attack > 5);
+
